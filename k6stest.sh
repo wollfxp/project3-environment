419253e347a7fa0d60854b297ee41ab9539ba1a5
wollfxp <wollfxp@gmail.com>
2020-08-19 17:11:08 +0300

    latest env changes

--- k6stest.sh
+++ k6stest.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker rm k6s
-docker run --network=host --name k6s -i loadimpact/k6 run --vus 20  --duration 30s --out influxdb=http://localhost:8086/db0 - <test.js
+docker run --network=host --name k6s -i loadimpact/k6 run --vus 100  --duration 30s --out influxdb=http://localhost:8086/db0 - <test.js
